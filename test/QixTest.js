fa5b2039b74184f1f8e723a972b8074a54a8a5d2
rthoth <ronaldo.silva@lemaf.ufla.br>
2014-08-08 11:41:09 -0300

    Spec tests are more correct

--- test/QixTest.js
+++ test/QixTest.js
@@ -4,6 +4,10 @@ var expect = tc.expect;
 
 var Qix = require('../lib/Qix');
 
+function asc(a, b) {
+	return a - b;
+}
+
 
 function context(file, description, fn) {
 	it(description, function(done) {
@@ -27,39 +31,60 @@ describe('Qix', function() {
 		}
 	]));
 
-	context('br.sp.qix', 'teste', function(qix, done) {
+	context('br.sp.qix', 'Sertãozinho / SP (-48.01596,-47.95535,-21.14624,-21.09490)', function(qix, done) {
 
 		this.timeout(0);
-		var spy, indexes = [];
+		var spy, foundIndexes = [], expectedIndexes = [106,185,229,276,284,356,408,441,448,487,553,576].sort(asc);
+		
 		qix.query(tc.factory.envelope(-48.01596,-47.95535,-21.14624,-21.09490), spy = tc.sinon.spy(tc.fix(function(err, index) {
 
-			/*console.log('callCount = %d, index=%d', spy.callCount, index);*/
-
 			if (spy.callCount >= 13) {
+				expect(err).to.be.null;
+				expect(index).to.be.null;
+				expect(foundIndexes.sort(asc)).to.eql(expectedIndexes);
 				done();
 			} else {
 				expect(err).to.be.null;
 				expect(index).to.be.gt(0);
-				indexes.push(index);
+				foundIndexes.push(index);
 			}
 		})));
 	});
 
 
-	context('br.qix', 'Paramirim (-42.23482,-41.84175,-13.55158,-13.44922)', tc.fix(function(qix, done) {
+	context('br.qix', 'Érico Cardoso / BA (-42.23482,-41.84175,-13.55158,-13.44922)', tc.fix(function(qix, done) {
 		var spy;
+
+		var expectedIndexes = [
+			71, 172, 305, 490,
+			727, 777, 1010,1224,
+			1537,1729,1772,1798,
+			2023,2587,2807,2999,
+			3030,3264,3812,3888,
+			3981,3995,4052,4204,
+			4212,4249,4326,4354,
+			4477,4527,4616,4708,
+			4718,5395],
+		foundIndexes = [];
 
 		qix.query(tc.factory.envelope(-42.23482,-41.84175,-13.55158,-13.44922), spy = tc.sinon.spy(function(err, index) {
 			expect(err).to.be.null;
 
 			if (spy.callCount >= 35) {
+				expect(err).to.be.null;
+				expect(index).to.be.null;
+				expect(foundIndexes.sort(asc)).to.be.eql(expectedIndexes);
 				done();
+			} else {
+				expect(err).to.be.null;
+				expect(index).to.be.gte(0);
+				foundIndexes.push(index);
 			}
 		}));
 	}));
 
 
-	context('br.qix', 'Maranhão (-58.39654,-48.58000,-9.69570,0.01552)', function(qix, done) {
+	context('br.qix', 'MA (-58.39654,-48.58000,-9.69570,0.01552)', function(qix, done) {
 		var spy, indexes = [];
 
 		qix.query(tc.factory.envelope(-58.39654,-48.58000,-9.69570,0.01552), spy = tc.sinon.spy(tc.fix(function(err, index) {
