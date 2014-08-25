3b7ddc2f7bb432b6fbc20616229a40cef6f52b86
rthoth <ronaldo.asilva@gmail.com>
2014-08-24 23:45:30 -0300

    Pre docs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // expose ShapeFile class
 var ShapeFile = exports.ShapeFile = require('./lib/ShapeFile');
 
+exports.Qix = require('./lib/Qix');
+exports.Shp = require('./lib/Shp');
+exports.Shx = require('./lib/Shx');
 
 // sugar method
 exports.intersects = function(fileName, geometry, options, callback) {
